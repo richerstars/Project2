98b04d1c4e1c98b780fccd0304d5eef47d542595
richerstars <81115334+richerstars@users.noreply.github.com>
2021-11-24 22:18:11 +0200

    add to webpack babel + html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, './src/index.js'),
+        main: path.resolve(__dirname, './src/app.js'),
     },
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            { test: /\.svg$/, use: 'svg-inline-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.(js)$/, use: 'babel-loader' },
+
+        ]
+    },
+    plugins: [
+        //new ESLintPlugin(options),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+    ],
+
 }
