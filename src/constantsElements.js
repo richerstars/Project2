a87431629c501471720af0f89c7c11b672cf588b
constMikhailPletniov <mihailpl1995@gmail.com>
2021-11-26 00:49:43 +0200

    I create mainPage, add logo and addEventListeners in index.js with empty functions

--- src/constantsElements.js
+++ src/constantsElements.js
@@ -1,21 +1,30 @@
 export const elementsOfDom = {
     mainClassWrapper: document.querySelector(selectorsCss.classWrapper),
-    formIdAuthorizedForm: document.querySelector(selectorsCss.idAuthorizedForm),
-    buttonIdbtnContinue: document.querySelector(selectorsCss.idbtnContinue),
-    AllinputStyle: document.querySelectorAll('.inpStyle'),
-    inputIdinpSubmit: document.querySelector(selectorsCss.idInpSubmit),
-    inputIdPassword: document.querySelector(selectorsCss.idPassword),
     navHeaderNav: document.querySelector(selectorsCss.classHeaderNav),
-    divClassLogo: document.querySelector(selectorsCss.classLogo)
+    divClassLogo: document.querySelector(selectorsCss.classLogo),
+    inputIdInputSearch: document.querySelector(selectorsCss.idInputSearch),
+    imgIdFilters: document.querySelector(selectorsCss.idFilters),
+    imgIdThemes: document.querySelector(selectorsCss.idThemes),
+    imgIdAuth: document.querySelector(selectorsCss.idAuth),
+    sectionclassPopularFilms: document.querySelector(selectorsCss.classPopularFilms),
+    allDivClassPopularFilmsItem: document.querySelectorAll(selectorsCss.classPopularFilmsItem),
+    allDivClassFilmsPaginationItem: document.querySelectorAll(selectorsCss.classFilmsPaginationItem),
+    buttonIdPreviousBtn: document.querySelector(selectorsCss.idPreviousBtn),
+    buttonIdNextBtn: document.querySelector(selectorsCss.idNextBtn)
 };
 
 const selectorsCss = {
     classWrapper: '.wrapper',
     idAuthorizedForm: '#authorizedForm',
-    idbtnContinue: '#btnContinue',
-    classInpStyle: '.inpStyle',
-    idInpSubmit: '#inpSubmit',
-    idPassword: '#password',
     classHeaderNav: '.headerNav',
-    classLogo: '.logo'
+    classLogo: '.logo',
+    idInputSearch: '#inputSearch',
+    idFilters: '#filters',
+    idThemes: '#themes',
+    idAuth: '#auth',
+    classPopularFilms: '.popularFilms',
+    classPopularFilmsItem: '.popularFilmsItem',
+    classFilmsPaginationItem: '.filmsPaginationItem',
+    idPreviousBtn: '#previousBtn',
+    idNextBtn: '#nextBtn'
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("Test")
+import { elementsOfDom } from "./constantsElements";
+
+elementsOfDom.inputIdInputSearch.addEventListener('click', findFilms);
+elementsOfDom.imgIdFilters.addEventListener('click', getFilters);
+elementsOfDom.imgIdThemes.addEventListener('click', changeTheme);
+elementsOfDom.imgIdAuth.addEventListener('click', getAuth);
+elementsOfDom.buttonIdNextBtn.addEventListener('click', getNextPaginationFilmItem);
+elementsOfDom.buttonIdPreviousBtn.addEventListener('click', getPreviousFilmItem);
+
+for (const item of elementsOfDom.allDivClassFilmsPaginationItem) {
+    item.addEventListener('click', getCurrentFilm);
+}
+
+for (const item of elementsOfDom.allDivClassPopularFilmsItem) {
+    item.addEventListener('click', getCurrentFilm);
+}
+
+function findFilms() {
+
+}
+
+function getFilters() {
+
+}
+
+function changeTheme() {
+
+}
+
+function getAuth() {
+
+}
+
+function getCurrentFilm() {
+
+}
+
+function getNextPaginationFilmItem() {
+
+}
+
+function getPreviousFilmItem() {
+
+}
