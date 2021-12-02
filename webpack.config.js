7a3ff5da058ee6da20c420c3ee1e0929e3def08b
constMikhailPletniov <mihailpl1995@gmail.com>
2021-12-02 23:43:20 +0200

    bug fixed in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
@@ -15,16 +16,17 @@ module.exports = {
     mode: 'development',
     module: {
 
-        rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] },
-        { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', },
+        rules: [{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
         {
-            test: /\.(png|jpe?g|gif|svg)$/i, use: [{ loader: 'file-loader', options: '[name].[ext]', },],
+            test: /\.(png|jpe?g|gif|svg|jpg)$/i, use: [{ loader: 'file-loader', options: { name: '/img/[name].[ext]', } },],
 
         },
+
         ]
     },
 
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({ template: './src/mainScreen.html' }),
     ],
 }
