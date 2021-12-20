62871eeec1586ffc5786d971d7e412d1d50fe4a8
richerstars <81115334+richerstars@users.noreply.github.com>
2021-12-20 20:25:48 +0200

    ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const path = require( 'path' )
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const { CleanWebpackPlugin } = require( "clean-webpack-plugin" );
+const ESLintPlugin = require( 'eslint-webpack-plugin' );
+const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
 
 module.exports = {
     entry: {
-        main: './src/scripts/index.js',
+        main: './src/scripts/index.ts',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve( __dirname, 'dist' ),
         filename: '[name].js',
         clean: true,
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    devServer: {
+        port: 4200,
     },
     mode: 'development',
     module: {
 
         rules: [
             {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+                test: /\.css$/i,
+                use: [ "style-loader", "css-loader" ],
             },
             {
                 test: /\.(png|jpe?g|gif|svg|jpg)$/i,
@@ -27,17 +35,30 @@ module.exports = {
                 test: /\.html$/i,
                 loader: "html-loader",
             },
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
         ]
     },
 
     plugins: [
 
-        new HtmlWebpackPlugin({
+        new HtmlWebpackPlugin( {
             filename: "index.html",
             template: "./src/pages/mainScreen.html",
-            chunks: ['main']
-        }),
+            chunks: [ 'main' ]
+        } ),
 
         new CleanWebpackPlugin(),
+        new ESLintPlugin( {
+            extensions: [ 'ts' ],
+        } ),
+        new ForkTsCheckerWebpackPlugin( {
+            typescript: {
+                configFile: path.resolve( __dirname, './tsconfig.json' ),
+            },
+        } ),
     ],
 }
